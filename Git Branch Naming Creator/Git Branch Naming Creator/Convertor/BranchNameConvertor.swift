b30421f726411aba38c0c47e20caae0b03a5608b
Andrew Galushka <andrewgalushka@yahoo.com>
2019-03-21 18:50:22 +0200

    Second step of main convert method moved to separated method

--- Git Branch Naming Creator/Git Branch Naming Creator/Convertor/BranchNameConvertor.swift
+++ Git Branch Naming Creator/Git Branch Naming Creator/Convertor/BranchNameConvertor.swift
@@ -13,20 +13,16 @@ class BranchNameConvertor: BranchNameConvertorType {
     func covert(text: String) throws -> String {
         var resultString = ""
         
+        // Trim whitespeses and new lines
         let trimmedString = text.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
         resultString = trimmedString
-        
-        
-        let regex = try NSRegularExpression(pattern: "\\s+|-")
-        
-        let stringWithUnderline = NSMutableString(string: resultString)
-        
-        regex.replaceMatches(in: stringWithUnderline, options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSRange.init(location: 0, length: stringWithUnderline.length), withTemplate: "_")
-        resultString = String(stringWithUnderline)
+
+        // Replace multy spaces and dash with "_"
+        resultString = try self.replaceMultySpacesAndDashWithUnderline(in: resultString)
         
         let insertUnderlineBetweenNumbersRegex = try NSRegularExpression(pattern: "\\d+")
         
-        let results = insertUnderlineBetweenNumbersRegex.matches(in: resultString, options: .reportCompletion, range: NSRange.init(location: 0, length: stringWithUnderline.length))
+        let results = insertUnderlineBetweenNumbersRegex.matches(in: resultString, options: .reportCompletion, range: NSRange.init(location: 0, length: resultString.count))
         
         for result in results {
             
@@ -40,15 +36,24 @@ class BranchNameConvertor: BranchNameConvertorType {
         let removeMultyUnderlineRegex = try NSRegularExpression(pattern: "_+")
         
         let stringWithoutMultyUnderline = NSMutableString(string: resultString)
-        removeMultyUnderlineRegex.replaceMatches(in: stringWithoutMultyUnderline, options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSRange.init(location: 0, length: stringWithUnderline.length), withTemplate: "_")
+        removeMultyUnderlineRegex.replaceMatches(in: stringWithoutMultyUnderline, options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSRange.init(location: 0, length: resultString.count), withTemplate: "_")
         resultString = String(stringWithoutMultyUnderline)
         
-        let unallowedSymobolsRegex =  try NSRegularExpression(pattern: "[^a-zA-Z_\\d]")
+        let unallowedSymobolsRegex = try NSRegularExpression(pattern: "[^a-zA-Z_\\d]")
         
         let stringWithoutUnallowdSumbols = NSMutableString(string: resultString)
         unallowedSymobolsRegex.replaceMatches(in: stringWithoutUnallowdSumbols, options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSRange.init(location: 0, length: stringWithoutUnallowdSumbols.length), withTemplate: "")
         resultString = String(stringWithoutUnallowdSumbols)
         
         return resultString.lowercased()
+    }
+    
+    private func replaceMultySpacesAndDashWithUnderline(in text: String) throws -> String {
+        let regex = try NSRegularExpression(pattern: "\\s+|-")
+        let stringWithUnderline = NSMutableString(string: text)
+        
+        regex.replaceMatches(in: stringWithUnderline, options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSRange.init(location: 0, length: stringWithUnderline.length), withTemplate: "_")
+        
+        return String(stringWithUnderline)
     }
 }
