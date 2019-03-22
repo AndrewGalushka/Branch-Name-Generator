3c284ba9616a28a8ae56a97de4b4a0159839b70c
Andrew Galushka <andrewgalushka@yahoo.com>
2019-03-22 11:10:21 +0200

    Added Tests target

---
+++ Git Branch Naming Creator/Git Branch Naming Creator Tests/Git_Branch_Naming_Creator_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Git_Branch_Naming_Creator_Tests.swift
+//  Git Branch Naming Creator Tests
+//
+//  Created by Galushka on 3/22/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import XCTest
+
+class GitBranchNamingCreatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
