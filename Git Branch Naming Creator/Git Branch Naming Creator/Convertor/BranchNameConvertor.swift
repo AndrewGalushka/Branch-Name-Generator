232e6d00adb7eaa3b03dafdc8bbf4c3d81564574
Andrew Galushka <andrewgalushka@yahoo.com>
2019-03-22 11:12:25 +0200

    Logs removed

--- Git Branch Naming Creator/Git Branch Naming Creator/Convertor/BranchNameConvertor.swift
+++ Git Branch Naming Creator/Git Branch Naming Creator/Convertor/BranchNameConvertor.swift
@@ -18,24 +18,16 @@ class BranchNameConvertor: BranchNameConvertorType {
         resultString = trimmedString
 
         // Replace multy spaces and dash with "_"
-        print("Before replaceMultySpacesAndDashWithUnderline --- \(resultString)")
         resultString = try self.replaceMultySpacesAndDashWithUnderline(in: resultString)
-        print("After replaceMultySpacesAndDashWithUnderline --- \(resultString)")
         
         // Insert underline between digit and letter
-        print("Before insertUnderlineBetweenDigitAndLetter --- \(resultString)")
         resultString = try self.insertUnderlineBetweenDigitAndLetter(in: resultString)
-        print("After insertUnderlineBetweenDigitAndLetter --- \(resultString)")
         
         // Remove unallowed symbols
-        print("Before removeUnallowedSymbols --- \(resultString)")
         resultString = try self.removeUnallowedSymbols(in: resultString)
-        print("After removeUnallowedSymbols --- \(resultString)")
         
         // Remove multy underline
-        print("Before removeMultyUnderline --- \(resultString)")
         resultString = try self.removeMultyUnderline(in: resultString)
-        print("After removeMultyUnderline --- \(resultString)")
         
         return resultString.lowercased()
     }
