cf21fea00e02749e0a288ca1bcee8317b66773b0
Andrew Galushka <andrewgalushka@yahoo.com>
2019-03-25 11:33:59 +0200

    Update test data

--- Git Branch Naming Creator/Git Branch Naming Creator Tests/GitBranchConvertorTestsData.swift
+++ Git Branch Naming Creator/Git Branch Naming Creator Tests/GitBranchConvertorTestsData.swift
@@ -10,23 +10,23 @@ import Foundation
 
 struct GitBranchConvertorTestsData {
     
-    static let testData1 = TestData(input: "VA_2846 Improve Matrix presentation  ",
-                                correctResult: "va_2846_improve_matrix_presentation")
+    static let testData1 = TestData(input: "GA_2846 Improve Matrix presentation  ",
+                                correctResult: "ga_2846_improve_matrix_presentation")
     
-    static let testData2 = TestData(input: "  VA 2846 impRove Matrix PResentation",
-                                    correctResult: "va_2846_improve_matrix_presentation")
+    static let testData2 = TestData(input: "  GA 2846 impRove Matrix PResentation",
+                                    correctResult: "ga_2846_improve_matrix_presentation")
     
-    static let testData3 = TestData(input: "NIS-4359 Do not show-Error Alert      second Time  Bug Fix",
-                                    correctResult: "nis_4359_do_not_show_error_alert_second_time_bug_fix")
+    static let testData3 = TestData(input: "GIS-4359 Do not show-Error Alert      second Time  Bug Fix",
+                                    correctResult: "gis_4359_do_not_show_error_alert_second_time_bug_fix")
     
-    static let testData4 = TestData(input: "1NIS4359do----Not show error_ALERT 2times in a row3 ",
-                                    correctResult: "1_nis_4359_do_not_show_error_alert_2_times_in_a_row_3")
+    static let testData4 = TestData(input: "1GIS4359do----Not show error_ALERT 2times in a row3 ",
+                                    correctResult: "1_gis_4359_do_not_show_error_alert_2_times_in_a_row_3")
     
-    static let testData5 = TestData(input: " #%^&&# NIS4359^##$*&^do----%!@#^Not show #!%______--@^ error!@%__%^#ALERT 2times in a row^^#$$^^&\n",
-                                    correctResult: "nis_4359_do_not_show_error_alert_2_times_in_a_row")
+    static let testData5 = TestData(input: " #%^&&# GIS4359^##$*&^do----%!@#^Not show #!%______--@^ error!@%__%^#ALERT 2times in a row^^#$$^^&\n",
+                                    correctResult: "gis_4359_do_not_show_error_alert_2_times_in_a_row")
     
-    static let testData6 = TestData(input: " #%^&&#13NIS4359^##$*&^do----%!@#^Not show #!%______--@^ error!@%__%^#ALERT 2times in a row^^#$$^^&\n",
-                                    correctResult: "13_nis_4359_do_not_show_error_alert_2_times_in_a_row")
+    static let testData6 = TestData(input: " #%^&&#13GIS4359^##$*&^do----%!@#^Not show #!%______--@^ error!@%__%^#ALERT 2times in a row^^#$$^^&\n",
+                                    correctResult: "13_gis_4359_do_not_show_error_alert_2_times_in_a_row")
 }
 
 extension GitBranchConvertorTestsData {
